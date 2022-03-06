2146d360a69193944975908b6a984ae818fced33
Incredible-06 <andyrodriguez0604@gmail.com>
2022-03-05 22:05:46 -0500

    make it better

---
+++ news-page1/main.js
@@ -0,0 +1,18 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 400});
+ScrollReveal().reveal('.cards-banner-two', {delay: 400});
+
+
+// For mabile
+
+ScrollReveal().reveal('.news-1', {delay: 300});
+ScrollReveal().reveal('.news-2', {delay: 300});
+ScrollReveal().reveal('.news-3', {delay: 300});
+ScrollReveal().reveal('.news-4', {delay: 300});
+
+
